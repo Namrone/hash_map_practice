8914fcc80b548b7115603eaef5112c8f8618e2e4
Namrone <151798569+Namrone@users.noreply.github.com>
2025-06-17 22:16:52 -0700

    Changed loop_buckets to dig into each bucket so the yield is each item in the bucket. Completed set method and tested/works with adding 1 item so far

--- hash_map.rb
+++ hash_map.rb
@@ -5,7 +5,7 @@ class HashMap
   def initialize
     @load_factor = 0.8
     @capacity = 0
-    @buckets = Array.new(16)
+    @buckets = Array.new(16){[]}
   end
 
   def hash(key)
@@ -18,8 +18,42 @@ class HashMap
   end
 
   def set(key, value)
+    #If current amount of buckets is over the max * load_factor then double the amount of buckets
+    if @capacity > @buckets.length*@load_factor
+      new_len = @bucket.length * 2
+      expansion = Array.new(new_len){[]}
+      
+      #Rehash
+      loop_buckets do |entry| 
+          code = hash(entry[0])%new_len
+          
+          expansion[code] << entry
+      end
+      
+      @buckets = expansion
+    end
+    
     code = hash(key) % @buckets.length
-    @capacity += 1 if @buckets[code].empty?
-    @buckets[code] = value
+    
+    #Checks if there is already the same key, if so then change only the value
+    if @buckets[code]
+      @buckets[code].each do |entry|
+        next unless entry[0] == key
+        entry[1] = value 
+        return
+      end
+    end
+
+    @capacity += 1
+    @buckets[code] << [key, value]
+  end
+
+  def loop_buckets
+    @buckets.each do |bucket|
+      next if bucket.empty?
+      bucket.each do |entry|
+        yield(entry)
+      end
+    end
   end
 end
